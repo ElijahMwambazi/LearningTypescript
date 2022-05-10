fe41f7392432c66f41874a4b2d6416f34c217877
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2022-05-10 06:43:57 +0200

    Added linked list sort test code

--- sort/src/script.ts
+++ sort/src/script.ts
@@ -1,8 +1,11 @@
 import { NumbersCollection } from "./NumbersCollection";
 import { CharactersCollection } from "./CharactersCollection";
+import { LinkedList } from "./LinkedList";
 import { Sorter } from "./Sorter";
 
+// *******************
 // Sorting Numbers
+// *******************
 const numbersCollection = new NumbersCollection([20, -5, 0, -84, 2, 37, 27, 8]);
 const numberSorter = new Sorter(numbersCollection);
 
@@ -13,7 +16,9 @@ console.log(`After: ${numbersCollection.data}`);
 
 console.log("\n");
 
+// *******************
 //  Sorting Characters
+// *******************
 const charactersCollection = new CharactersCollection(
     "likewhatintheactualfuck"
 );
@@ -23,3 +28,25 @@ console.log("Sorting Charaters:");
 console.log(`Before: ${charactersCollection.data}`);
 characterSorter.sort();
 console.log(`After: ${charactersCollection.data}`);
+
+console.log("\n");
+
+// *******************
+// Sorting Linked List
+// *******************
+const linkedList = new LinkedList();
+
+// Adding values to linked list
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const linkedListSorter = new Sorter(linkedList);
+
+console.log("Sorting Linked List:");
+console.log("Before:");
+linkedList.print();
+linkedListSorter.sort();
+console.log("After:");
+linkedList.print();
