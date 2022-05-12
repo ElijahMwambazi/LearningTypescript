48a28d266379ed150a11215d634b82d3adb57faf
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2022-05-12 07:01:45 +0200

    Recafored to make csvfilereader reusable with abrstract class and generics

--- stats/src/CsvFileReader.ts
+++ stats/src/CsvFileReader.ts
@@ -1,12 +1,10 @@
 import fs from "fs";
-import { dateStrintToDate } from "./utils";
-import { MatchResult } from "./matchResult";
-
-type MatchData = [Date, string, string, number, number, MatchResult, string];
-export class CsvFileReader {
-    data: MatchData[] = [];
+export abstract class CsvFileReader<T> {
+    data: T[] = [];
 
     constructor(public fileName: string) {}
+
+    abstract mapRow(row: string[]): T;
 
     read(): void {
         this.data = fs
@@ -17,16 +15,6 @@ export class CsvFileReader {
             .map(function (row: string): string[] {
                 return row.split(",");
             })
-            .map(function (row: string[]): MatchData {
-                return [
-                    dateStrintToDate(row[0]),
-                    row[1],
-                    row[2],
-                    parseInt(row[3]),
-                    parseInt(row[4]),
-                    row[5] as MatchResult,
-                    row[6],
-                ];
-            });
+            .map(this.mapRow);
     }
 }
