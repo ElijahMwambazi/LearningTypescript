662510f049f95b33fe1a66242a064c1f1f1c157f
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2022-05-20 01:14:24 +0200

    Re-intergrated Eventing, Sync and Attributes

--- web/src/models/User.ts
+++ web/src/models/User.ts
@@ -1,72 +1,21 @@
-import axios, { AxiosResponse } from "axios";
+import { Eventing } from "./Eventing";
+import { Sync } from "./Sync";
+import { Attributes } from "./Attributes";
 
-interface UserProperties {
+export interface UserProps {
   id?: number;
   firstName?: string;
   lastName?: string;
   age?: number;
-  gender?: "male" | "female";
-  email?: string;
-  phoneNumber?: number;
 }
 
-type PropertyName = [string, number];
-type Callback = () => void;
-
+const rootUrl = "http://localhost:3000/users";
 export class User {
-  events: { [key: string]: Callback[] } = {};
+  public event: Eventing = new Eventing();
+  public sync: Sync<UserProps> = new Sync<UserProps>(rootUrl);
+  public attributes: Attributes<UserProps>;
 
-  constructor(private data: UserProperties) {}
-
-  // Retrieve user data
-  get(propertyName: string): PropertyName {
-    return this.data[propertyName];
-  }
-
-  // Set user data
-  set(update: UserProperties): void {
-    Object.assign(this.data, update);
-  }
-
-  // Register user events
-  on(eventName: string, callback: Callback): void {
-    const handlers = this.events[eventName] || [];
-    handlers.push(callback);
-    this.events[eventName] = handlers;
-  }
-
-  // Trigger user events
-  trigger(eventName: string): void {
-    const handlers = this.events[eventName];
-
-    if (!handlers || handlers.length === 0) {
-      return;
-    }
-
-    handlers.forEach((callback): void => {
-      callback();
-    });
-  }
-
-  // Fetch user data
-  fetch(): void {
-    axios
-      .get(`http://localhost:3000/users/${this.get("id")}`)
-      .then((response: AxiosResponse): void => {
-        this.set(response.data);
-      });
-  }
-
-  // Save user data
-  save(): void {
-    const id = this.get("id");
-
-    if (id) {
-      // Update user data
-      axios.put(`http://localhost:3000/users/${id}`, this.data);
-    } else {
-      // Create new user
-      axios.post("http://localhost:3000/users/", this.data);
-    }
+  constructor(attrs: UserProps) {
+    this.attributes = new Attributes<UserProps>(attrs);
   }
 }
