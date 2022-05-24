7f8b9c069122d9451433ac13208d09f33ac7ef87
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2022-05-24 08:28:56 +0200

    Implemented view nesting

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,5 +1,5 @@
 import { User } from "./models/User";
-import { UserForm } from "./views/UserForm";
+import { UserEdit } from "./views/UserEdit";
 
 const user = User.buildUser({
   name: "Elijah",
@@ -9,9 +9,11 @@ const user = User.buildUser({
 const root = document.getElementById("root");
 
 if (root) {
-  const userForm = new UserForm(root, user);
+  const userEdit = new UserEdit(root, user);
 
-  userForm.render();
+  userEdit.render();
+
+  console.log(userEdit);
 } else {
   throw new Error("Root element not found");
 }
