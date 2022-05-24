9221797645f37b0a9a5a28f0d03b3b552c520421
Elijah Mwambazi <elijahmwambaxi@gmail.com>
2022-05-24 09:10:22 +0200

    Implemented Collection view

--- web/src/index.ts
+++ web/src/index.ts
@@ -1,19 +1,20 @@
-import { User } from "./models/User";
-import { UserEdit } from "./views/UserEdit";
+import { UserList } from "./views/UserList";
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
 
-const user = User.buildUser({
-  name: "Elijah",
-  age: 100,
+const users = new Collection(
+  "http://localhost:3000/users",
+  (json: UserProps) => {
+    return User.buildUser(json);
+  }
+);
+
+users.on("change", () => {
+  const root = document.getElementById("root");
+
+  if (root) {
+    new UserList(root, users).render();
+  }
 });
 
-const root = document.getElementById("root");
-
-if (root) {
-  const userEdit = new UserEdit(root, user);
-
-  userEdit.render();
-
-  console.log(userEdit);
-} else {
-  throw new Error("Root element not found");
-}
+users.fetch();
